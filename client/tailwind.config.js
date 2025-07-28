f09c66ea9987f297d90db1282cfe193c06378d32
Suyash Upadhyay <uranium235.a@gmail.com>
2025-07-28 22:32:56 +0530

    feat: Initial project setup with client and server structure

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This is the crucial line
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5001; 
+
+
+app.use(cors({
+  origin: 'http://localhost:5173' 
+}));
+
+app.use(express.json());
+
+
+app.get('/api/health', (req: Request, res: Response) => {
+  res.status(200).send({ status: 'OK', message: 'Server is healthy and running!' });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
