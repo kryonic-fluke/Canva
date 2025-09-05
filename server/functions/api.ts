33341fae5f76dbf4b6945d9d59bbbe6d87ba9e40
Suyash Upadhyay <uranium235.a@gmail.com>
2025-09-05 23:10:02 +0530

    refactor : add middleware in api.ts

--- server/functions/api.ts
+++ server/functions/api.ts
@@ -1,6 +1,10 @@
 import serverless from 'serverless-http';
-import { app, connectToDatabase } from '../src/index';
+import { app as apiRoutes, connectToDatabase } from '../src/index';
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import express from 'express';
+
+const app = express();
+app.use('/api', apiRoutes);
 
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
